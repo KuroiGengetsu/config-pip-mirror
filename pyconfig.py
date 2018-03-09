c6db3a5ba580cb1e39f69cb0068ec2f35fa59a48
KuroiGengetsu <1198148133@qq.com>
2018-03-09 22:24:32 +0800

    change a lot

--- pyconfig.py
+++ pyconfig.py
@@ -11,17 +11,17 @@ trusted-host = mirrors.aliyun.com
 
 
 def main():
-    if sys.platform == "win32":                                   # if on windows
-        homepath = os.path.expandvars("%userprofile%")            # The userhome's path
-        if not os.path.exists(homepath + "\\pip"):                # check whether the folder named pip existed
-            os.makedirs(homepath + "\\pip")                       # if not, create it
-        with open(homepath + "\\pip\\pip.ini", "w") as fp:        # create the pip.ini file
-            fp.write(CONTENT)                                     # write CONTENT
-    elif sys.platform == 'linux':                                 # if on linux
-        if not os.path.exists('~/.pip'):                           # check whether the folder named .pip existed
-            os.makedirs("~/.pip")                                 # if not, create it
-        with open("~/.pip/pip.conf", "w") as fp:                  # create the pip.conf file
-            fp.write(CONTENT)                                     # write CONTENT
+    if sys.platform == 'win32':
+        homepath = os.path.expandvars("%userprofile%" + "\\pip")
+        filename = '\\pip.ini'
+    else:
+        homepath = '~/.pip'
+        filename = '/pip.conf'
+
+    if not os.path.exists(homepath):
+        os.makedirs(homepath)
+    with open(homepath + filename, 'w') as write_file:
+        write_file.write(CONTENT)
 
 
 if __name__ == '__main__':
